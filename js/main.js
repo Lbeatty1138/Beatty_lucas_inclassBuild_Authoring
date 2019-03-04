a6af1f4a4a31ca0ab4f6ead727104eab8d71ff81
Luke Beatty <lukebeatty@Lukes-MBP.fc.ca>
2019-03-04 16:08:02 -0500

    adding js to make the animation work right giving it the UX the site needs

--- js/main.js
+++ js/main.js
@@ -10,11 +10,32 @@
 		// console.log(audio);
 		// checking for a matching audi element, then kill function
 		if (!audio) { return; } 
+		//  ^ is a "not" operater or a bang
 
 		
 		audio.currentTime = 0;
 		audio.play();
+
+		// grab the div on every click and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add('playing');
 	}
+
+	function removePlayingClass(event) {
+		// debugger;
+
+		if (event.propertyName !== "transform") {
+			return;
+		} else {
+			// remove the playing class here from the active div
+			console.log('transform is done!', `${event.propertyName}`);
+			event.target.classList.remove('playing');
+		}	
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 
 	window.addEventListener("keydown", playDrumKitSound);
 
